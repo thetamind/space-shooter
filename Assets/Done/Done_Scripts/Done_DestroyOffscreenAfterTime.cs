6abb46641fadf85cc9e223b338cc769dc7fadd75
Matthew Boehlig <thetamind@gmail.com>
2015-05-01 16:49:46 -0500

    Destroy hazards offscreen after timeout

---
+++ Assets/Done/Done_Scripts/Done_DestroyOffscreenAfterTime.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Done_DestroyOffscreenAfterTime : MonoBehaviour
+{
+	public float lifetime;
+
+	void Start()
+	{
+		// Remove screen wrapper and let object collide with boundary
+		Object target = GetComponent<ScreenWrap>();
+		if (target == null) target = gameObject;
+
+		Destroy(target, lifetime);
+	}
+}
